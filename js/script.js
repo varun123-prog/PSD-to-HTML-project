d625aa4bc5f68a60d57ac14f626219d8f704ee99
Varun Kumar <kumarvarun5181@gmail.com>
2022-09-13 00:08:15 +0530

    I have created a website using psd template(HTML,CSS,JS)

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// for open the menu bar at hamburger
+function openMenu(){
+    document.getElementById("navbar").style.left= "0px";
+}
+
+// for closing the menu bar at hamburger
+function closeMenu(){
+    document.getElementById("navbar").style.left= "-280px";
+}
